18ffe87fd0b851706824fde62dba30754d012008
Philippe Bolduc <pbolduc2354@gmail.com>
2018-05-21 18:19:53 +0000

    Adds conditionals exercices

---
+++ go-outside.js
@@ -0,0 +1,12 @@
+var temperature = -12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeve won't cut it!");
+} else {
+  console.log("Short sleeve are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
